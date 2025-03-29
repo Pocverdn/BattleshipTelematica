44e15e74de68b7971712706d8113d63a525b4add
Pocverdn <ssanchezc5@eafit.edu.co>
2025-03-29 16:57:26 -0500

    Refactorización de server

--- server.c
+++ server.c
@@ -14,6 +14,15 @@
 #define BUFFER_SIZE 1024
 #define BUFFER_SIZE_Confirm 1
 
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+
+typedef struct {
+    int server_fd;
+    sockaddr_in address;
+} Server;
+
+
 void *handle_client(void *client_socket) {
     int new_socket = *(int *)client_socket;
     char buffer[BUFFER_SIZE] = {0};
@@ -39,59 +48,63 @@ void *handle_client(void *client_socket) {
     return NULL;
 }
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+int setup_server(Server *server) {
+    server->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server->server_fd == -1) {
+        perror("Socket creation failed");
+        return -1;
+    }
+    
+    int opt = 1;
+    if (setsockopt(server->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+        perror("Setsockopt failed");
+        return -1;
+    }
 
+    server->address.sin_family = AF_INET;
+    server->address.sin_addr.s_addr = INADDR_ANY;
+    server->address.sin_port = htons(PORT);
+
+    if (bind(server->server_fd, (sockaddr *)&server->address, sizeof(server->address)) < 0) {
+        perror("Bind failed");
+        return -1;
+    }
+    
+    if (listen(server->server_fd, 3) < 0) {
+        perror("Listen failed");
+        return -1;
+    }
+
+    printf("Server is listening on port %d...\n", PORT);
+    return 0;
+}
+
+void accept_clients(Server *server) {
+    int addrlen = sizeof(server->address);
     pthread_t thread_id;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("Socket creation failed");
-        return 1;
-    }
-
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        perror("Setsockopt failed");
-        return 1;
-    }
-
-        
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        return 1;
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        return 1;
-    }
-        
-    printf("Server is listening on port %d...\n", PORT);
-
     while (1) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        int new_socket = accept(server->server_fd, (sockaddr *)&server->address, (socklen_t *)&addrlen);
         if (new_socket < 0) {
             perror("Accept failed");
             continue;
         }
-
-
         if (pthread_create(&thread_id, NULL, handle_client, (void *)&new_socket) != 0) {
             perror("Thread creation failed");
+            close(new_socket);
             continue;
         }
-
         pthread_detach(thread_id);
     }
+}
 
-    close(server_fd);  
 
+int main() {
+    Server server;
+    if (setup_server(&server) < 0) {
+        return 1;
+    }
+    accept_clients(&server);
+    close(server.server_fd);
     return 0;
 }
