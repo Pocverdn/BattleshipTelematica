87b1c361983a6d24e6c950323394cce093de95d2
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-04-08 21:33:36 -0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 //#define _UNIX03_THREADS
 #include <pthread.h>
-
+#include <time.h>
 
 
 using namespace std;
 
-void timed_in() {
-
+void* timed_in(void* att) {
+    unsigned char* mgs = (unsigned char*)att;
+    scanf("%d%d", mgs[0], mgs[1]);
+    return NULL;
 }
 
 int main(int argc, char* argv[])
 {
+    static unsigned char att[2];
+    void* arg;
+    //unsigned char* att;
+    att[0] = 0;
+    att[1] = 0;
+    arg = att;
 
     pthread_t thread_id;
-    pthread_create(&thread_id, NULL, timed_in(), (void*)args); //-1 si a acabado
-    int pthread_cancel(pthread_t thread);
+    //pthread_create(&thread_id, NULL, timed_in(), (att); //-1 si a acabado 
+    pthread_create(&thread_id, NULL, timed_in, arg); //-1 si a acabado
+
+    time_t tiempo;
+    time_t current;
+    time(&tiempo);
+    tiempo = tiempo + 5;
+    do {
+        time(&current);
+    } while ((current < tiempo) & (!att[1]));
+    pthread_cancel(thread_id);
+    cout << att;
 
     return 0;
 }
