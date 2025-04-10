e3528274a61a8a9b8b6d67f3a63356bf52d4385d
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-04-09 21:25:32 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -401,7 +401,7 @@ void game(int sock, char board[SIZE][SIZE], ship ships[TOTAL_SHIPS], char enemyB
                     //cout << (tiempo - current) << "\r";
                 } while ((current < tiempo) & (x[1]==247));
             pthread_cancel(thread_id);
-            
+            x[1] = (x[1] == 247 ? 0 : x[1]);
             } while (x[0] > SIZE || x[1] > SIZE);//while (x[0] < 0 || x[0] > SIZE || x[1] < 0 || x[1] > SIZE);
 
             att.posX = x[0];
